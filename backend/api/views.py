490eb7e136f965ef6caf2d8a7123d7998f787ed4
Ivan Arkhangelsky <largo.v.88@mail.ru>
2025-03-06 22:54:38 +0300

    Add workflow with PEP8

--- backend/api/views.py
+++ backend/api/views.py
@@ -3,6 +3,7 @@ from django.db.models import Sum
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
 from djoser import views as djoser_views
+
 from djoser.serializers import SetPasswordSerializer
 from rest_framework import permissions, status, viewsets
 from rest_framework.decorators import action
